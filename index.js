b8d9a88ad7741cb22ada1f7c1ff5b363ba20a8ab
SteveMustanski <steve.mustanski@gmail.com>
2023-11-24 16:10:57 -0500

    added simple routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url')
 
 // // this section is blocking / synchronous 
 // const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -24,7 +25,21 @@ const http = require('http');
 // http server
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from the http server!');
+  console.log(req.url)
+
+  const pathName = req.url;
+
+  if(pathName === '/' || pathName === '/overview') {
+    res.end('This is the overview!');
+
+  } else if(pathName === '/product') {
+  res.end('This is the product!');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
